7e49bee141b2600365df82082d0b318d0e4df8b4
ChristophSchock <34169998+ChristophSchock@users.noreply.github.com>
2017-12-02 11:45:39 +0100

    Update day2a.js

--- day2a.js
+++ day2a.js
@@ -1,5 +1,5 @@
 let input = "538 74 948";
-let numbers = input.split(' ');			// split input into an array of strings containing sequences
+let numbers = input.split(' ');				// split input into an array of strings containing sequences
 var checksum = 0;
 var digits = []
 
@@ -7,9 +7,9 @@ var digits = []
 for (var i=0; i<=(numbers.length-1);i++) {
 	digits = numbers[i].split('');		// split sequences into digits
 	digits = digits.map(Number);
-	let biggest = Math.max(digits);		// looking for the highest integer
-	let smallest = Math.min(digits);	// looking for the smalest integer
-	checksum+=(biggest-smallest);		// adding the difference to checksum
+	let biggest = Math.max.apply(null, digits);		// looking for the highest integer
+	let smallest = Math.min.apply(null, digits);		// looking for the smalest integer
+	checksum+=(biggest-smallest);			// adding the difference to checksum
 }
 
 console.log(checksum);
