255ee0442d74b55179841305c44ec7c4d6e6fd1d
ChristophSchock <34169998+ChristophSchock@users.noreply.github.com>
2017-12-02 13:05:15 +0100

    Solution

--- day2a.js
+++ day2a.js
@@ -1,11 +1,11 @@
-let input = "538	74	948";
-let numbers = input.split('\t');				// split input into an array of strings containing sequences
+let input = "83 434 32 54 223\n932 343 494 99";
+let numbers = input.split('\n');				// split input into an array of strings containing rows
 var checksum = 0;
 var digits = []
 
 
 for (var i=0; i<=(numbers.length-1);i++) {
-	digits = numbers[i].split('');		// split sequences into digits
+	digits = numbers[i].split('\t');		// split sequences into digits
 	digits = digits.map(Number);
 	let biggest = Math.max.apply(null, digits);		// looking for the highest integer
 	let smallest = Math.min.apply(null, digits);		// looking for the smalest integer
